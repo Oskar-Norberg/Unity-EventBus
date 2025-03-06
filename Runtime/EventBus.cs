e20b6384d560e438e448bc1b6d65bf5955be9db7
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-03-06 20:26:24 +0100

    EDIT: [Script] Optimize raising events

--- Runtime/EventBus.cs
+++ Runtime/EventBus.cs
@@ -5,6 +5,7 @@ namespace ringo.EventSystem
 {
     public static class EventBus
     {
+        // TODO: Make sure this resets on domain reload
         private static Dictionary<Type, List<EventHandler<IEvent>>> _handlers = new();
         
         public static void Subscribe<T>(EventHandler<IEvent> handler) where T : IEvent
@@ -19,15 +20,11 @@ namespace ringo.EventSystem
         
         public static void Publish<T>(T @event) where T : IEvent
         {
-            foreach (var keyValuePair in _handlers)
+            var handlers = _handlers[@event.GetType()];
+
+            foreach (var handler in handlers)
             {
-                foreach (EventHandler<IEvent> handler in keyValuePair.Value)
-                {
-                    if (handler.GetType() == typeof(T))
-                    {
-                        handler.Handle(@event);
-                    }
-                }
+                handler.Handle(@event);
             }
         }
     }
